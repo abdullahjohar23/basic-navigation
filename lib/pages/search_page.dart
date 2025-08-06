faf956333e3f21227e339bf02998f90f14ed7284
Abdullah Johar <jrrabdullah23@gmail.com>
2025-08-06 12:54:03 +0600

    capitalized the name

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class Searchpage extends StatelessWidget {
-    const Searchpage({super.key});
+class SearchPage extends StatelessWidget {
+    const SearchPage({super.key});
 
     @override
     Widget build(BuildContext context) {
